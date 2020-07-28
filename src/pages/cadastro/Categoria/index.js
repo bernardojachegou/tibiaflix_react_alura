f9b442c9c7f7b0ee345288564fcf12a0ec610622
Michel Bernardo <bernardoenglish@outlook.com>
2020-07-28 18:49:18 -0400

    adição do form

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -5,9 +5,20 @@ import { Link } from 'react-router-dom';
 function CadastroCategoria() {
     return (
         <Template>
-            <h1>Cadastro de vídeos</h1>
+            <h1>Cadastro de categoria</h1>
 
-            
+            <form>
+                <label>
+                    Nome da Categoria:
+                    <input
+                        type="text"
+                    />
+                </label>
+
+                <button>
+                    Cadastrar
+                </button>
+            </form>
 
             <Link to="/">
                 Ir para a home
