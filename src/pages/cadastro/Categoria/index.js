fbf942263ff2680789521584360b11a41a9b8c6d
Michel Bernardo <bernardoenglish@outlook.com>
2020-07-30 05:46:52 -0400

    aplicação do useState

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Template from '../../../components/Template';
 import { Link } from 'react-router-dom';
 
 function CadastroCategoria() {
+
+    const [nomeDaCategoria, setNomeDaCategoria] = useState('Default');
+    const [categorias, setCategorias] = useState(["Teste"]);
+
+    function handleInputValue(event) {
+        setNomeDaCategoria(event.target.value);
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log("Tentou enviar o form...");
+        setCategorias([
+            ...categorias,
+            nomeDaCategoria
+        ])
+    }
+
     return (
         <Template>
-            <h1>Cadastro de categoria</h1>
+            <h1>Cadastro de categoria: {nomeDaCategoria}</h1>
 
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>
                     Nome da Categoria:
                     <input
                         type="text"
+                        value={nomeDaCategoria}
+                        onChange={handleInputValue}
                     />
                 </label>
 
@@ -19,6 +38,16 @@ function CadastroCategoria() {
                     Cadastrar
                 </button>
             </form>
+
+            <ul>
+                {categorias.map((categoria, index) => {
+                    return (
+                        <li key={`${categoria}${index}`}>
+                            {categoria}
+                        </li>
+                    )
+                })}
+            </ul>
 
             <Link to="/">
                 Ir para a home
