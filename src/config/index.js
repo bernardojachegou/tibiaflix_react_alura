81d050cfa7e4ac35a6984ff1add4a0b89c607d2c
Michel Bernardo <bernardoenglish@outlook.com>
2020-07-31 16:51:56 -0400

    adição da url do backend

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable linebreak-style */
 const URL_BACKEND = window.location.hostname.includes('localhost')
   ? 'http://localhost:8080'
-  : 'http://devsoutinhoflix.herokuapp.com';
+  : 'https://kurt-tibiaflix.herokuapp.com';
 
 export default {
   URL_BACKEND,
