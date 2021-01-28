76f33c78fd101b5a5c810d7c3c853aa07a0457fd
Michel Bernardo <bernardoenglish@outlook.com>
2021-01-28 06:02:16 -0400

    just reviewing the project flux to remember some features

--- src/index.js
+++ src/index.js
@@ -5,19 +5,16 @@ import './index.css';
 import Home from './pages/Home';
 import CadastroVideo from './pages/cadastro/Video';
 import CadastroCategoria from './pages/cadastro/Categoria';
-import Watch from './pages/Watch';
 import ErrorPage from './pages/ErrorPage';
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route path="/" component={Home} exact />
-      <Route path="/watch" component={Watch} />
       <Route path="/cadastro/video" component={CadastroVideo} exact />
       <Route path="/cadastro/categoria" component={CadastroCategoria} exact />
       <Route component={ErrorPage} />
     </Switch>
   </BrowserRouter>,
-  document.getElementById('root'),
-
+  document.getElementById('root')
 );
